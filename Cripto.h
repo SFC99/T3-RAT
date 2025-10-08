4de73958e4809d71437c09a942daef2e632d5962
S0WBR4 <133049076+S0WBR4@users.noreply.github.com>
2025-10-07 23:49:15 -0600

    Cripto.h

--- Cripto.h
+++ Cripto.h
@@ -1,13 +1,10 @@
 #ifndef CRIPTO_H
 #define CRIPTO_H
-
 #include <string>
 #include <vector>
-
 class Cripto {
 public:
     static std::string encriptar(const std::string& texto, const std::vector<unsigned char>& llave);
     static std::string desencriptar(const std::string& textoCifrado, const std::vector<unsigned char>& llave);
 };
-
 #endif
